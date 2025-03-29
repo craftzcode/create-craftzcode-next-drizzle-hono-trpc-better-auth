47edca9b554045d0afa3954f64a72ef18229895e
craftzcode <craftzcode.dev@gmail.com>
2025-03-29 22:20:24 +0800

    refactor(web): use HydrateClient and prefetch helper functions instead of queryClient

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -1,12 +1,19 @@
-import { getQueryClient, trpc } from '@rhu-ii/api/server'
+// import { getQueryClient, trpc } from '@rhu-ii/api/server'
+import { HydrateClient, prefetch, trpc } from '@rhu-ii/api/server'
 
-import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
+// import { dehydrate, HydrationBoundary } from '@tanstack/react-query'
 
 import { ClientGreeting } from '../components/client-greeting'
 
 export default async function Home() {
-  const queryClient = getQueryClient()
-  void queryClient.prefetchQuery(
+  // const queryClient = getQueryClient()
+  // void queryClient.prefetchQuery(
+  //   trpc.hello.queryOptions({
+  //     text: 'craftzcode'
+  //   })
+  // )
+
+  prefetch(
     trpc.hello.queryOptions({
       text: 'craftzcode'
     })
@@ -14,9 +21,9 @@ export default async function Home() {
 
   return (
     <div className='space-y-4 p-4'>
-      <HydrationBoundary state={dehydrate(queryClient)}>
+      <HydrateClient>
         <ClientGreeting />
-      </HydrationBoundary>
+      </HydrateClient>
     </div>
   )
 }
